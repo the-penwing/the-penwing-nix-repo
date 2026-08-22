5d8cc64bba181d1d19a252696f88492e3c3f3eb8
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-08-22 16:56:42 +1000

    added blackjack-slint

---
+++ pkgs/blackjack-slint.nix
@@ -0,0 +1,97 @@
+{
+  lib,
+  stdenv,
+  fetchgit,
+  rustPlatform,
+  pkg-config,
+  makeWrapper,
+  fontconfig,
+  libxkbcommon,
+  wayland,
+  wayland-protocols,
+  vulkan-loader,
+  libGL,
+  mesa,
+  libx11,
+  libxcursor,
+  libxi,
+  libxrandr,
+  darwin,
+}: let
+  src = fetchgit {
+    url = "https://github.com/the-penwing/blackjack-slint.git";
+    rev = "b468ff2547f094fb6e80b64c9307318a6799950b";
+    sha256 = "sha256-eKY9ZRfkeEgJSRkT61D9kmjFvv80UvohhXm9xRIyPa0=";
+  };
+
+  isDarwin = stdenv.hostPlatform.isDarwin;
+
+  linuxRuntimeLibs = [
+    libxkbcommon
+    wayland
+    vulkan-loader
+    fontconfig
+    libGL
+    mesa
+    libx11
+    libxcursor
+    libxi
+    libxrandr
+  ];
+
+  linuxBuildInputs = [
+    fontconfig
+    libxkbcommon
+    wayland
+    wayland-protocols
+    vulkan-loader
+    libGL
+    mesa
+    libx11
+    libxcursor
+    libxi
+    libxrandr
+  ];
+
+  darwinBuildInputs = with darwin.apple_sdk.frameworks; [
+    AppKit
+    QuartzCore
+    Metal
+    Foundation
+  ];
+in
+  rustPlatform.buildRustPackage {
+    pname = "blackjack-slint";
+    version = "0.1.0";
+
+    inherit src;
+
+    cargoLock = {
+      lockFile = "${src}/Cargo.lock";
+      outputHashes = {
+        "blackjack-rs-0.1.0" = "sha256-zYsRzYM6wRskJcch4bORWCicHrsTAbowvZmJkHwuL1s=";
+      };
+    };
+
+    nativeBuildInputs = [pkg-config makeWrapper];
+    buildInputs =
+      if isDarwin
+      then darwinBuildInputs
+      else linuxBuildInputs;
+
+    postInstall = ''
+      mkdir -p $out/share/blackjack-slint
+      cp -r ${src}/assets $out/share/blackjack-slint/assets
+
+      wrapProgram $out/bin/blackjack-slint \
+        --set BLACKJACK_ASSETS_DIR $out/share/blackjack-slint/assets \
+        ${lib.optionalString (!isDarwin)
+        "--set LD_LIBRARY_PATH ${lib.makeLibraryPath linuxRuntimeLibs}"}
+    '';
+
+    meta = {
+      description = "Blackjack GUI built with Slint";
+      homepage = "https://github.com/the-penwing/blackjack-slint";
+      platforms = lib.platforms.unix;
+    };
+  }
