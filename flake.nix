b2559105db71d8da2ca15592ab0f9b1600001d9b
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-08-25 22:26:23 +1000

    added cachix to flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
-  description = "penwing systems package repository";
-
+  description = "the-penwing package repository";
+  nixConfig = {
+    extra-substituters = ["https://the-penwing.cachix.org"];
+    extra-trusted-public-keys = ["the-penwing.cachix.org-1:b1OsSQhQU/bhmwDiMuStEovsvUIzVPYVfZt289btWAg="];
+  };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-
   outputs = inputs @ {
     flake-parts,
     nixpkgs,
@@ -13,7 +15,6 @@
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
-
       perSystem = {
         pkgs,
         system,
